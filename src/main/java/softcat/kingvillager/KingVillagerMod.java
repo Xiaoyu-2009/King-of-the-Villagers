e4113e078d8d57544605c405666af588d9c0582e
小宇不是鱼 <3551371733@qq.com>
2025-12-13 21:06:42 +0800

    Update KingVillagerMod.java

--- src/main/java/softcat/kingvillager/KingVillagerMod.java
+++ src/main/java/softcat/kingvillager/KingVillagerMod.java
@@ -32,37 +32,31 @@ import softcat.kingvillager.Block.ThroneBlock;
 import static softcat.kingvillager.KingVillagerMod.MOD_ID;
 
 @Mod(MOD_ID)
-public class KingVillagerMod {
-
+公共 class KingVillagerMod {
     public static final String MOD_ID = "kingvillager";
     private static final Logger LOGGER = LogManager.getLogger();
 
-    // Create DeferredRegister objects
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
     public static final DeferredRegister<PoiType> POI_TYPES = DeferredRegister.create(ForgeRegistries.POI_TYPES, MOD_ID);
     public static final DeferredRegister<VillagerProfession> VILLAGER_PROFESSIONS = DeferredRegister.create(ForgeRegistries.VILLAGER_PROFESSIONS, MOD_ID);
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
-
-    // Register blocks
+    
     public static final RegistryObject<Block> THRONE = BLOCKS.register("throne", 
         () -> new ThroneBlock(BlockBehaviour.Properties.of()
-            .mapColor(MapColor.METAL)
-            .requiresCorrectToolForDrops()
-            .strength(4.0F)
-            .noOcclusion()));
+            。mapColor(MapColor.METAL)
+            。requiresCorrectToolForDrops()
+            。strength(4.0F)
+            。noOcclusion()));
 
-    // Register items
     public static final RegistryObject<Item> THRONE_ITEM = ITEMS.register("throne", 
         () -> new BlockItem(THRONE.get(), new Item.Properties()));
-
-    // Register POI types
+    
     public static final RegistryObject<PoiType> KING_POI = POI_TYPES.register("kingpoi",
         () -> new PoiType(
             ImmutableSet.copyOf(THRONE.get().getStateDefinition().getPossibleStates()),
             1, 1));
 
-    // Register professions
     public static final RegistryObject<VillagerProfession> KING_PROFESSION = VILLAGER_PROFESSIONS.register("king",
         () -> new VillagerProfession("king", 
             holder -> holder.is(KING_POI.getKey()),
@@ -73,26 +67,17 @@ public class KingVillagerMod {
 
     public KingVillagerMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
-        // Register DeferredRegisters to the mod event bus
         BLOCKS.register(modEventBus);
         ITEMS.register(modEventBus);
         POI_TYPES.register(modEventBus);
         VILLAGER_PROFESSIONS.register(modEventBus);
         CREATIVE_MODE_TABS.register(modEventBus);
-        
-        // Register event listener for creative tab contents
         modEventBus.addListener(this::addCreative);
-        
-        // Register server and other game events
         MinecraftForge.EVENT_BUS.register(this);
-        
         LOGGER.info("King Villager Mod initialized!");
     }
-    
-    // Add items to creative tabs
+
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        // Add to functional blocks tab only
         if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
             event.accept(THRONE_ITEM);
         }
